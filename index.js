14bce377b89180b214aa9142b9617e7a9ef33c80
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-01-25 19:52:22 +0200

    Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
+
+async function main() {
+    mongoose.set('strictQuery', false);
+
+    await mongoose.connect('mongodb://127.0.0.1:27017/CatShelter')
+
+    console.log(`DataBase Connected`)
+}
+main();

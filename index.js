3be69588fcbb57293242f3e77d8610200adf5ee1
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-01-25 23:55:51 +0200

    Exporting Modules

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
 const mongoose = require('mongoose');
-const catSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Nedd from NAME!'],//Може да си опишем съобщението за грешката
-        minLength: 4,
-        maxLength: 6,
-        lowercase: false,
-        uppercase: true
-    },
-    age: {
-        type: Number,
-        min: 1,
-        max: 9,
-    },
-    breed: String,
-});
-//Method
-catSchema.methods.sayHello = function () {
-    console.log(`Hello, my name is ${this.name} and mew!`);
-};
 
-//Virtual property
-catSchema.virtual('info').get(function () {
-    return `${this.name} - age ${this.age}  - breed ${this.breed}`;
-});
-
-//Validation methods or Customs validation
-catSchema.path('name').validate(function () {
-    return this.name.startsWith('N');
-}, 'Name should start with N')
-
-
-const Cat = mongoose.model('Cat', catSchema);
+const Cat = require('./models/Cat.js');
 
 async function main() {
     mongoose.set('strictQuery', false);
@@ -48,7 +17,7 @@ async function main() {
         //e property и не се извиква като метод ()
     });
 
-    await saveCat("Kiki", 6, 'pgushko');//валидация на name
+    await saveCat("niki", 6, 'pgushko');//валидация на name
     //await saveCat('Mishi', 3, 'pgushko');
     await readCats();
 
---
+++ models/Cat.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const catSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Nedd from NAME!'],//Може да си опишем съобщението за грешката
+        minLength: 4,
+        maxLength: 6,
+        lowercase: false,
+        uppercase: true
+    },
+    age: {
+        type: Number,
+        min: 1,
+        max: 9,
+    },
+    breed: String,
+});
+//Method
+catSchema.methods.sayHello = function () {
+    console.log(`Hello, my name is ${this.name} and mew!`);
+};
+
+//Virtual property
+catSchema.virtual('info').get(function () {
+    return `${this.name} - age ${this.age}  - breed ${this.breed}`;
+});
+
+//Validation methods or Customs validation
+catSchema.path('name').validate(function () {
+    return this.name.startsWith('N');
+}, 'Name should start with N')
+
+
+const Cat = mongoose.model('Cat', catSchema);
+module.exports = Cat;
+//или
+//module.exports = mongoose.model('Cat', catSchema);

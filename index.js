e0517248538cdac2ea39112b8d04d7a008a081bc
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-01-25 22:17:41 +0200

    Add save, read

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const mongoose = require('mongoose');
+const catSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    breed: String,
+});
+
+const Cat = mongoose.model('Cat', catSchema);
 
 async function main() {
     mongoose.set('strictQuery', false);
 
-    await mongoose.connect('mongodb://127.0.0.1:27017/CatShelter')
+    await mongoose.connect('mongodb://127.0.0.1:27017/catShelter')
+    console.log(`DataBase Connected`);
 
-    console.log(`DataBase Connected`)
+    await saveCat('Mishi', 3, 'pgushko');
+    await readCats();
+
 }
+async function readCats() {
+    const cats = await Cat.find();
+    console.log(cats);
+}
+
+async function saveCat(name, age, breed) {
+
+    await Cat.create({
+        name,
+        age,
+        breed,
+    });
+
+    // const cat = new Cat({//инстанция на модела 
+    //     name,
+    //     age,
+    //     breed,
+    // })
+    // await cat.save();//запазва в базата данни
+}
+
 main();
+
